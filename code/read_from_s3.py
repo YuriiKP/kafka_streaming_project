35f6e2ced2f29582064c891b7d6656dff4ac67a0
CrazyAndLucky <2kyup2@gmail.com>
2025-11-09 16:53:44 +1000

    Добавил read_from_s3.py

---
+++ code/read_from_s3.py
@@ -0,0 +1,99 @@
+import os
+
+import duckdb
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+conn = duckdb.connect(database=":memory:")
+
+count_of_rows = conn.sql(
+    f"""
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_url_style = 'path';
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_access_key_id = '{ACCESS_KEY}';
+    SET s3_secret_access_key = '{SECRET_KEY}';
+    SET s3_use_ssl = FALSE;
+
+    SELECT
+        COUNT(*) AS cnt
+    FROM
+        's3://prod-python/*/*.parquet';
+    """,
+)
+
+print(count_of_rows)
+
+df_schema_of_model = conn.sql(
+    f"""
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_url_style = 'path';
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_access_key_id = '{ACCESS_KEY}';
+    SET s3_secret_access_key = '{SECRET_KEY}';
+    SET s3_use_ssl = FALSE;
+
+    SELECT path_in_schema, column_id FROM parquet_metadata('s3://prod-python/*/*.parquet');
+    """,
+)
+
+print(df_schema_of_model)
+
+first_10_rows = conn.sql(
+    f"""
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_url_style = 'path';
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_access_key_id = '{ACCESS_KEY}';
+    SET s3_secret_access_key = '{SECRET_KEY}';
+    SET s3_use_ssl = FALSE;
+
+    SELECT
+        *
+    FROM
+        's3://prod-python/*/*.parquet'
+    LIMIT 10;
+    """,
+)
+
+print(first_10_rows)
+
+
+first_10_rows_1_event_id = conn.sql(
+    f"""
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_url_style = 'path';
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_access_key_id = '{ACCESS_KEY}';
+    SET s3_secret_access_key = '{SECRET_KEY}';
+    SET s3_use_ssl = FALSE;
+
+    SELECT
+        "event_params.event_type_id" AS event_type_id,
+        "event_params.event_type" AS event_type,
+        "event_params.user_id" AS user_id,
+        "event_params.platform_token" AS platform_token,
+        "event_params.ipv4" AS ipv4,
+        "event_params.country" AS country,
+        "event_params.uuid_track" AS uuid_track,
+        "event_timestamp_ms.ts" AS ts,
+        "event_timestamp_ms.ts_ms" AS ts_ms,
+    FROM
+        's3://prod-python/*/*.parquet'
+    WHERE
+        1=1
+        AND "event_params.event_type_id" = 1
+    LIMIT 10;
+    """,
+)
+
+print(first_10_rows_1_event_id)
